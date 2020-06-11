e5dfbafa157601abed808e37e94411ca7adb24c8
shyamdhawale <32157028+shyamdhawale@users.noreply.github.com>
2020-06-12 00:02:19 +0530

    Change the 12 instead of 0

--- index.js
+++ index.js
@@ -18,7 +18,16 @@ function clock()
 		{sal='Good Morning';} 
 	if(hour>=12 &&hour<17 &&minute<60 &&second<60 ){
 		sal='Good Afternoon';} if(hour>=17 &&hour<24 &&minute<60 &&second<60 )
-		{sal='Good Evening';} if(hour>=12){hour=hour-12;ap='PM';}else{ap='AM';} 
+		{sal='Good Evening';} 
+		if (hour == 0){
+			hour = 12;
+		}
+		if(hour>12){
+			hour=hour-12;
+				ap='PM';
+			}else{
+				ap='AM';
+			} 
 		if(minute<10){minute="0"+minute} if(second<10){second="0"+second};
 document.getElementById('time').innerHTML=hour+" : "+ minute +" : "+second+" " +ap; 
 document.getElementById('da').innerHTML=dayname;

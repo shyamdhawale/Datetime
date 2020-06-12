a4069b4af6e22f6629eaa2093f544cc12d3d24eb
shyamdhawale <32157028+shyamdhawale@users.noreply.github.com>
2020-06-12 11:03:20 +0530

    correct date function

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function clock(){
 	var options = { weekday: 'long', month: 'long', day: 'numeric' };
 	var today  = new Date();
 	document.getElementById('time').innerHTML = today.toLocaleTimeString("en-US"); 
-	document.getElementById('da').innerHTML= today.toLocaleDateString("hi-IN", options);
+	document.getElementById('date').innerHTML= today.toLocaleDateString("hi-IN", options);
 	setTimeout("clock()",60);
 }
 window.onload=function(){clock();}
